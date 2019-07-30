b232301c7d3ffe39d3e691fa10ea8a4270c2b9b6
Wai Kiet William Leung <leungwkw@gmail.com>
2019-07-30 17:30:53 -0500

    Initial Commit

---
+++ Budget App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Budget App
+//
+//  Created by William Leung on 7/30/19.
+//  Copyright © 2019 William Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
